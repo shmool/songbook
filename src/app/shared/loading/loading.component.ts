e7917fa68877793a1f7a0ad811092650b313ba0c
Shmuela <shmuela@500tech.com>
2018-06-06 03:27:18 +0300

    change loading spinner

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loading',
   template: `
-    <div class="flex-column">
-      <div class="flex-row">
-        <mat-spinner color="accent"></mat-spinner>
-      </div>
-    </div>
+    <div class="loader"></div>
   `,
   styleUrls: ['./loading.component.scss']
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
-import { MatIconModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
 import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatIconModule,
-    MatProgressSpinnerModule
+    MatIconModule
   ],
   declarations: [
     AvatarComponent,
