354bf617e7fcea1af97afb2b078de19b2d1c0444
shmool <shmuelaj@gmail.com>
2019-05-15 18:57:55 +0300

    fix link element

--- src/app/main/song-list/song-list.component.ts
+++ src/app/main/song-list/song-list.component.ts
@@ -5,15 +5,13 @@ import { SongsService } from '../songs.service';
   selector: 'app-song-list',
   template: `
     <mat-list>
-      <mat-list-item>
-        <a>
-          <button mat-mini-fab>
-            <mat-icon>note_add</mat-icon>
-          </button>
-        </a>
-      </mat-list-item>
+      <a mat-list-item>
+        <button mat-mini-fab>
+          <mat-icon>note_add</mat-icon>
+        </button>
+      </a>
       <a mat-list-item *ngFor="let item of songList$"
-                    class="router-link">{{ item.title }}
+         class="router-link">{{ item.title }}
       </a>
     </mat-list>
   `,
@@ -21,8 +19,8 @@ import { SongsService } from '../songs.service';
 })
 export class SongListComponent implements OnInit {
   songList$ = [
-    {id: 1, title: 'Hello'},
-    {id: 2, title: 'World'}
+    { id: 1, title: 'Hello' },
+    { id: 2, title: 'World' }
   ];
 
   constructor() {
