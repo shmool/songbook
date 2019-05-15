8c5b9eaa0134bbc894fdfd338cb8024ac43106f8
shmool <shmuelaj@gmail.com>
2019-05-15 19:46:17 +0300

    step 3 solution - get song

--- src/app/main/song/song.component.ts
+++ src/app/main/song/song.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SongsService } from '../songs.service';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-song',
   template: `
-    <div class="song">
+    <div class="song" *ngIf="song$ | async as song">
       <h2>{{ song.title }}</h2>
       <a routerLink="edit">
         <button class="edit-btn" mat-mini-fab color="accent">
@@ -19,12 +20,16 @@ import { SongsService } from '../songs.service';
   styleUrls: ['./song.component.scss']
 })
 export class SongComponent implements OnInit {
-  song = { id: 1, title: 'Heal the World', performer: 'Michael Jackson', lyrics: 'Heal the world \n Make it a better place' };
+  song$;
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private songService: SongsService) {
   }
 
   ngOnInit() {
+    this.song$ = this.route.params.pipe(
+      map(params => params.id),
+      switchMap(id => this.songService.getSong(id))
+    );
   }
 
 }
